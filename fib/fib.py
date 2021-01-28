b5d6d42974a64e5eb7af0ad963bcde62a0bb7ac9
jis_on_windows <seiji.kishida@gmail.com>
2021-01-28 21:40:03 +0900

    added fib

--- fib/fib.py
+++ fib/fib.py
@@ -1,7 +1,7 @@
 def fib(n):
-    if n == 0 or n == 1:
+    if n == 1 or n == 2:
         return 1
     else:
-        return fib(n - 1) +fib(n - 2)
+        return fib(n - 1) + fib(n - 2)
 
-fib(5)
+print(fib(8))
